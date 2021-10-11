e6d9ca117e6a1019e674fd28de1a2f6f8d111d42
Bob DeBlaey <deblaeyboba@johndeere.com>
2021-10-11 16:30:59 -0500

    tweaking show and edit pages

---
+++ controllers/configs.js
@@ -0,0 +1,6 @@
+const Country = require('../models').Country;
+const Producer = require('../models').Producer;
+const Seller = require('../models').Seller;
+const Note = require('../models').Note;
+
+
